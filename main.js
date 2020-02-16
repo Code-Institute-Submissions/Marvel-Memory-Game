45a0083564c3180a068e77db38a2a322fe4befca
NickBell123 <nick.bell2008@gmail.com>
2020-02-16 14:39:01 +0000

    if else statements added to check for card match, toggling related classes

--- main.js
+++ main.js
@@ -1,13 +1,39 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let isCardFlipped = false;
+let fistCard;
+let secondCard;
 
 // Flip card with a click
-function flipCard(){
-    console.log('i was clicked!');
-    console.log(this);
+function flipCard() {
     this.classList.toggle('flip');
+
+    if (!isCardFlipped) {
+        //first click
+        isCardFlipped = true;
+        fistCard = this;
+    } else {
+        //second click
+        isCardFlipped = false;
+        secondCard = this;
+
+        //do cards match?
+        // console.log(fistCard.dataset.image);
+        // console.log(secondCard.dataset.image);
+        if (fistCard.dataset.image === secondCard.dataset.image) {
+            fistCard.removeEventListener('click', flipCard);
+            secondCard.removeEventListener('click', flipCard);
+        } else {
+            //does not match
+            setTimeout(function () {
+                fistCard.classList.remove('flip');
+                secondCard.classList.remove('flip');
+            }, 1500);
+        }
+
+    }
 }
 
-cards.forEach(function(card){
+cards.forEach(function (card) {
     card.addEventListener('click', flipCard);
 });
