7aba4ebbbb9be315a9cde6c7d5d3ac64512f7386
NickBell123 <nick.bell2008@gmail.com>
2020-02-17 16:49:11 +0000

    refactoring js code and added Lockcard fucntion to fix bug

--- main.js
+++ main.js
@@ -1,37 +1,60 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let isCardFlipped = false;
+let lockCards = false;
 let fistCard;
 let secondCard;
 
 // Flip card with a click
 function flipCard() {
+    if(lockCards) return;
     this.classList.toggle('flip');
 
     if (!isCardFlipped) {
         //first click
         isCardFlipped = true;
         fistCard = this;
-    } else {
-        //second click
-        isCardFlipped = false;
-        secondCard = this;
 
-        //do cards match?
-        // console.log(fistCard.dataset.image);
-        // console.log(secondCard.dataset.image);
-        if (fistCard.dataset.image === secondCard.dataset.image) {
-            fistCard.removeEventListener('click', flipCard);
-            secondCard.removeEventListener('click', flipCard);
-        } else {
-            //does not match
-            setTimeout(function () {
-                fistCard.classList.remove('flip');
-                secondCard.classList.remove('flip');
-            }, 1500);
-        }
-
+        return;
     }
+    //second click
+    isCardFlipped = false;
+    secondCard = this;
+
+    //do cards match?
+
+    checkForMatch();
+
+}
+
+
+function checkForMatch() {
+    //ternary operator 
+    let doMatch = fistCard.dataset.image === secondCard.dataset.image;
+
+    doMatch ? disableCards() : unflipCards();
+    // if (fistCard.dataset.image === secondCard.dataset.image) {
+    //     disableCards();
+    // } else {
+    //     //does not match. Timeout used to view 2nd card
+    //     unflipCards();
+    // }
+}
+
+function disableCards() {
+    fistCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards() {
+    lockCards = true;
+
+    setTimeout(function () {
+        fistCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        lockCards = false;
+    }, 1500);
 }
 
 cards.forEach(function (card) {
